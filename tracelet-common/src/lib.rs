88d5a9dc85de299d5cb5ce039482199ce96e08df
lazzerex <nambinh236@gmail.com>
2026-09-19 14:20:00 +0700

    feat: export latency constants

--- tracelet-common/src/lib.rs
+++ tracelet-common/src/lib.rs
@@ -1,6 +1,8 @@
 pub mod event;
+pub mod latency;
 
 pub use event::{
     ExecEvent, OpenEvent, TcpEvent, AF_INET, AF_INET6, TCP_EVENT_ACCEPT, TCP_EVENT_CLOSE,
     TCP_EVENT_CONNECT,
 };
+pub use latency::{HIST_SLOTS, SYSCALL_COUNT, SYSCALL_NAMES};
