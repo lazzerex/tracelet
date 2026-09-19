78f52466f0f465c0aa2bf3c733f2841cfcbb5156
lazzerex <nambinh236@gmail.com>
2026-09-19 16:33:11 +0700

    feat: export dashboard event types

--- tracelet-common/src/lib.rs
+++ tracelet-common/src/lib.rs
@@ -1,10 +1,11 @@
 pub mod event;
 pub mod filter;
 pub mod latency;
+pub mod stream;
 
 pub use event::{
-    ExecEvent, OpenEvent, TcpEvent, AF_INET, AF_INET6, TCP_EVENT_ACCEPT, TCP_EVENT_CLOSE,
-    TCP_EVENT_CONNECT,
+    ExecEvent, OpenEvent, TcpEvent, AF_INET, AF_INET6, EVENT_KIND_EXEC, EVENT_KIND_OPEN,
+    TCP_EVENT_ACCEPT, TCP_EVENT_CLOSE, TCP_EVENT_CONNECT,
 };
 pub use filter::{
     FilterConfig, COMM_MAX_LEN, FILTER_EVENT_ACCEPT, FILTER_EVENT_ALL, FILTER_EVENT_CLOSE,
