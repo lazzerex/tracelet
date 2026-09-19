50034ab67ecbdabeb432d3ebe1d80fabc2a111e2
lazzerex <nambinh236@gmail.com>
2026-09-19 14:51:49 +0700

    feat: export filter configuration

--- tracelet-common/src/lib.rs
+++ tracelet-common/src/lib.rs
@@ -1,8 +1,13 @@
 pub mod event;
+pub mod filter;
 pub mod latency;
 
 pub use event::{
     ExecEvent, OpenEvent, TcpEvent, AF_INET, AF_INET6, TCP_EVENT_ACCEPT, TCP_EVENT_CLOSE,
     TCP_EVENT_CONNECT,
+};
+pub use filter::{
+    FilterConfig, COMM_MAX_LEN, FILTER_EVENT_ACCEPT, FILTER_EVENT_ALL, FILTER_EVENT_CLOSE,
+    FILTER_EVENT_CONNECT,
 };
 pub use latency::{HIST_SLOTS, SYSCALL_COUNT, SYSCALL_NAMES};
