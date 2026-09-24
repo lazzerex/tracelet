f034a6d4c86391856dd9cf4d0f5393299ccc19b4
lazzerex <nambinh236@gmail.com>
2026-09-24 19:40:46 +0700

    feat: extend shared filter configuration

--- tracelet-common/src/filter.rs
+++ tracelet-common/src/filter.rs
@@ -4,13 +4,16 @@ pub const FILTER_EVENT_CLOSE: u32 = 4;
 pub const FILTER_EVENT_ALL: u32 = 7;
 
 pub const COMM_MAX_LEN: usize = 15;
+pub const MAX_PIDS: usize = 16;
 
 #[repr(C)]
 #[derive(Debug, Clone, Copy, Default, PartialEq, Eq)]
 pub struct FilterConfig {
-    pub pid: u32,
-    pub pid_enabled: u32,
-    pub comm_enabled: u32,
     pub event_mask: u32,
+    pub pid_count: u32,
+    pub pids: [u32; MAX_PIDS],
+    pub ppid: u32,
+    pub ppid_enabled: u32,
+    pub comm_enabled: u32,
     pub comm: [u8; 16],
 }
