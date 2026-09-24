a1bbe8d0955fd8bda048e469d59924c7525f3997
lazzerex <nambinh236@gmail.com>
2026-09-25 02:19:25 +0700

    feat: extend shared latency contract

--- tracelet-common/src/latency.rs
+++ tracelet-common/src/latency.rs
@@ -3,3 +3,5 @@ pub const HIST_SLOTS: u32 = 32;
 pub const SYSCALL_COUNT: u32 = 3;
 
 pub const SYSCALL_NAMES: [&str; SYSCALL_COUNT as usize] = ["read", "write", "openat"];
+
+pub const LATENCY_SLOTS: u32 = HIST_SLOTS + 2;
